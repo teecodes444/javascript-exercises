718e121834b1b1a13371dac15fa80bd9415fafe9
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-12 14:14:25 -0500

    JS exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split('').reverse().join('')
 
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
