22b9519f05f29f9677052d68737d300192384a82
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-12 22:40:39 -0500

    Not completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,18 @@
 const repeatString = function(str, num) {
-for (let i = 1; i <= num; i++) {
-    console.log(str)
-}
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let string = '';
+
+    for (let i = 0; i < num; i++) {
+        string += str
+    }
+
+    return string
 };
 
-repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
