dd1427fbb07e529418d042f007a9a7f08345b2e6
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-12 21:53:10 -0500

    JS exercise complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+for (let i = 1; i <= num; i++) {
+    console.log(str)
+}
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,33 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+   let newArray = [];
+   
+   for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === arg) {
+        arr.slice(arr[i])
+    } else {
+        newArray.push(arr[i])
+    }
+
+   }
+
+   return newArray
+
 };
+
+removeFromArray([1, 2, 3, 4], 3)
+
+
+// CORRECT SOLUTION
+
+/* const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  }; */
 
 // Do not edit below this line
 module.exports = removeFromArray;
