0cd386d2d28729de59c3dc2bea9e243415a2f9ed
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-12 22:27:42 -0500

    Not completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    let num1 = Number(arg1)
+    let num2 = Number(arg2)
+    for (let i = 1; i >= num1 && i <= num2; i++ ) {
+        if (num1 > 0  && num2 > 0) {
+            sum = sum + i;
+        } else { 
+            return 'ERROR'
+        }
+    }
 
+    return sum
 };
 
 // Do not edit below this line
