ee7888eb56f402696df241f0901eb73ffa851c38
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-12 23:42:54 -0500

    JS exercise complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+
+  let temp;
+  temp = ((far - 32) * 5/9).toFixed(1)
+  let celTemp = Math.round(temp * 10) / 10
+  return celTemp
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let temp;
+  temp = (cel * 9/5 + 32)
+  let farTemp = Math.round(temp * 10) / 10
+  return farTemp
+
 };
 
 // Do not edit below this line
