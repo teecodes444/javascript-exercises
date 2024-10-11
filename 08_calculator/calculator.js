f51a4aeaa210e3579665a4520299d2eacb3b7515
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-10 20:46:49 -0500

    JS exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	/* let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i]
+  }
+  return total */
+
+  return array.reduce((a, i) => a + i, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, i) => a * i, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product *= i
+  }
+  return product;
 };
 
 // Do not edit below this line
