73a9bcbc1f07063b3013a8cb3c3ff2f201aaba93
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-10 21:12:34 -0500

    JS exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let reverseStr = cleanStr.split('').reverse().join('');
+    if (reverseStr === cleanStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
