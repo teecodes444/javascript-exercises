1c822f296c6abf059bd2cec31b68b3e5899476bf
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-12 23:22:06 -0500

    JS exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    const fib = [0,1];
+    let count = Number(n);
+    for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
 
+    return fib[count];
 };
 
 // Do not edit below this line
